d6a3dc9882efc71b72001fc753888db0ddb951fc
Roland Salloum <roland.salloum00@outlook.com>
2021-07-17 11:19:42 +0300

    chore: initial project setup

---
+++ src/main/java/com/rolandsalloum/personaldashboardbackend/PersonalDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rolandsalloum.personaldashboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDashboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalDashboardBackendApplication.class, args);
+    }
+
+}
