f9ef8e1dc66b8acfd7a13a5625a169a80a3602db
Roland Salloum <45897168+RolandSall@users.noreply.github.com>
2021-08-04 15:39:54 +0300

    Feature/configuration service (#2)
    * chore: configuration service to project
    
    * feat: properties for config service
    
    * feat[todo-service]: routing to config-service
    
    * feat[discovery-service]: system.properties file

--- configuration-service/src/test/java/com/rolandsalloum/configurationservice/ConfigurationServiceApplicationTests.java
+++ configuration-service/src/test/java/com/rolandsalloum/configurationservice/ConfigurationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rolandsalloum.discoveryservice;
+package com.rolandsalloum.configurationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServiceApplicationTests {
+class ConfigurationServiceApplicationTests {
 
     @Test
     void contextLoads() {
