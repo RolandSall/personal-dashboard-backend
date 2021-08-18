aae6bd383fe8378f399b07f4172fbc19b603aa57
Roland Salloum <roland.salloum00@outlook.com>
2021-08-18 10:17:16 +0300

    fix[todo-service]: default value for swagger todo request

--- todo-service/src/main/java/com/rolandsalloum/todoservice/controllers/TodoController/TodoApiRequest.java
+++ todo-service/src/main/java/com/rolandsalloum/todoservice/controllers/TodoController/TodoApiRequest.java
@@ -1,6 +1,8 @@
 package com.rolandsalloum.todoservice.controllers.TodoController;
 
 import com.rolandsalloum.todoservice.models.tasks.*;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ import java.util.List;
 @AllArgsConstructor
 @SuperBuilder
 public class TodoApiRequest {
+
+    @ApiModelProperty(required = true, example = "YYYY-MM-DD")
     private String date;
 
 
